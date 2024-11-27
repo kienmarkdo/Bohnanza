4a47d7e6396f987a7fa92f55f22ae90f73a1c265
Kien Do <kienmarkdo@gmail.com>
2024-11-27 18:52:44 -0500

    Updated README and play.sh
    Specified c++14 support is required

--- play.sh
+++ play.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the C++ code
-g++ -std=c++17 src/*.cpp -Iinclude -o Game
+g++ -std=c++14 src/*.cpp -Iinclude -o Game
 
 # Run the executable (works on macOS, Linux, and Windows with a Unix-like shell)
 ./Game
